a20f1f280861e08b20279198b20a079a48762023
Applh.com <github@applh.com>
2015-07-16 14:15:24 +0000

    add mvc/controller/classContent.php

---
+++ mvc/controller/classContentTag.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+* class:		ContentTag
+* creation:		2015-07-16 14:12:47
+*
+* author:		Long Hai
+* license:		All rights reserved
+*
+*/
+
+class ContentTag
+{
+	//-- ContentTag CLASS CODE BEGINS
+
+	//-- ATTRIBUTES
+
+
+	//-- METHODS
+
+	// CONSTRUCTOR
+	function __construct ()
+	{
+		// WRITE YOUR CODE HERE
+	}
+
+	//-- ContentTag CLASS CODE ENDS
+};
