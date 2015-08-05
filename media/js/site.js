e98509665224367a00801327bcb53d5244fe6447
Applh.com <github@applh.com>
2015-08-05 20:33:01 +0000

    test gsap effects

--- media/js/site.js
+++ media/js/site.js
@@ -3,8 +3,8 @@ var HK = {};
 HK.jinit = function ()
 {
     HK.btn = jQuery(".btn");
-    TweenMax.staggerFrom(HK.btn, 1, {opacity:0, scaleX:0, scaleY:0, rotation:360}, 1);
-
+    TweenMax.staggerFrom(HK.btn, 2, {opacity:0, scaleX:0, scaleY:0, rotation:360}, 1);
+    TweenMax.from(jQuery("#cover"), 2, {y:-500, ease: Bounce.easeOut});
 };
 
 jQuery(HK.jinit);
